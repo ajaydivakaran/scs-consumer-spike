80dbc5b6df8a71713981c19ce8b78ddcf13912ac
Ajay Divakaran <ajay_886@hotmail.com>
2017-02-18 13:36:28 +0530

    Removed boiler plate Kafka setup since Spring Boot 1.5.1 does the magic

--- src/main/java/org/egov/SmsNotificationListener.java
+++ src/main/java/org/egov/SmsNotificationListener.java
@@ -2,15 +2,15 @@ package org.egov;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SmsNotificationListener {
 
     @KafkaListener(id = "${kafka.topics.notification.sms.id}",
             topics = "${kafka.topics.notification.sms.name}",
             group = "${kafka.topics.notification.sms.group}")
     public void listen(ConsumerRecord<String, SmsRequest> record) {
-        System.err.println("***** received message [key " + record.key() + "] + value [" + record.value()
-                + "] from topic egov-notification-mail");
         SmsRequest request = record.value();
         System.out.println("Received SMS " + request);
     }
